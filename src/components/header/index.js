d8ae4e158704101a6dcbb233af000de962340648
shivam shrivastava <shivam.shrivastava1006@gmail.com>
2023-03-11 14:49:57 +0530

    header done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Header() {
    return (
-      <div>
-         This is Header
-      </div>
+      <>
+         <div className="flex justify-between p-5 shadow">
+            <div id='logo' className='p-1 font-bold'>
+               GPT Assistant
+            </div>
+
+            <div id="rightbar" className='flex gap-10'>
+               <div className='border border-solid rounded w-52 p-1'>
+                  Search Bar
+               </div>
+               <div className='border border-solid rounded-full w-9'>
+               </div>
+            </div>
+         </div>
+      </>
    )
 }
+
+
+// *Logo
+// *Search Bar
+// *Avatar 
